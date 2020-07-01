2e00a8529fa995e1eb92ab24f5dada8a988a8302
ilhanbozcan <ilhanbozcann@gmail.com>
2020-07-01 15:32:19 +0300

    1st commit

--- exchangeNameParser.py
+++ exchangeNameParser.py
@@ -3,10 +3,7 @@ from bs4 import BeautifulSoup
 import json
 
 url = 'https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html'
-
 page = requests.get(url)
-
-
 soup = BeautifulSoup(page.content, 'html.parser')
 
 exchange = {}
