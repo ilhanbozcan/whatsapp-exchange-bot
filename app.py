1cb7aba668f6eef30de080e8c3c91e8a01b207a3
ilhanbozcan <ilhanbozcann@gmail.com>
2020-07-01 13:59:28 +0300

    dolar

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 
 
+
 app = Flask(__name__)
 
 
@@ -14,17 +15,18 @@ def hello():
     return 'Welcome'
 
 
+names_list =[]
 
 @app.route('/sms', methods = ['POST'])
 def sms_reply():
     response = requests.get('https://api.exchangeratesapi.io/latest?base=USD')
     names = ""
-    names_list =[]
+
     msg = request.form.get('Body')
+    resp = MessagingResponse()
 
     if(msg=='Birimler'):
 
-        resp = MessagingResponse()
         count = 0
         for i in response.json()['rates'].keys():
             names_list.append(i)
@@ -35,13 +37,13 @@ def sms_reply():
                 count = 0
             count+=1
 
-
-        return(resp.message(names))
+        resp.message(names)
+        return(resp)
 
     elif str(msg) in names_list:
         response = requests.get('https://api.exchangeratesapi.io/latest?base={}'.format(msg))
-        resp = MessagingResponse()
-        return(resp.message(response.json))
+        resp.message(response.json)
+        return(resp)
 
 
 
@@ -51,9 +53,8 @@ def sms_reply():
 
 
     else:
-        resp = MessagingResponse()
         resp.message('You said: {}'.format(msg))
-        return(resp.message())
+        return(resp)
 
 
 
